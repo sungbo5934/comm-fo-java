e747add00ae48fd0a03355510daa777a3f77c72a
srec0043 <srec0043@SSPN-SREC004301.KoreaZinc.co.kr>
2021-07-16 11:54:32 +0900

    최초 커밋 공통 fo

---
+++ src/test/java/com/ssb/comm/CommFoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssb.comm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommFoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
